3acb8f74e3da517ff01708d38312d520fef4b5e1
DHANANJAYMewada <dhananjaymewada62@gmail.com>
2025-09-09 00:30:54 +0530

    Add files via upload

---
+++ modules.py
@@ -0,0 +1,13 @@
+'''a=9
+b=3
+c=a+b
+print(c)'''
+# arthmeticopreter
+'''a=8
+c=not a
+print(c)'''
+'''name="hsrry"
+print(name[0])'''
+# slicing with skip value
+name="gghwhwduhwh"
+print(name[2:4:2])
