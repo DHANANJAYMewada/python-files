87cf2368362b8c67036fb75718fd2cb03d10f449
DHANANJAYMewada <dhananjaymewada62@gmail.com>
2025-09-09 00:24:03 +0530

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,9 @@
+'''marks={"harry":100,
+       "shubham":20,
+       "rohan":30}
+print(marks,type(marks))'''
+words={ 
+       "madad":"help",
+       "kursi":"chai"}
+word=input("enter the word")
+print(words[word])
